5a15d769c642470c8e460e45c99cb18c6392554b
Manash.ch <manashchoudhary123@gmail.com>
2024-11-26 10:16:59 +0530

    first commit

---
+++ src/main/java/com/Java8Learning/Runners.java
@@ -0,0 +1,30 @@
+package com.Java8Learning;
+
+
+import com.Java8Learning.Repositerys.Employee1Repository;
+import com.Java8Learning.Services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Runners implements CommandLineRunner {
+    @Autowired
+    private Employee1Repository employee1Repository;
+
+    @Autowired
+    EmployeeService employeeService;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+//        List<Employee1> employee1s = employeeService.getallEmployeeSalary();
+
+
+    }
+
+
+
+}
