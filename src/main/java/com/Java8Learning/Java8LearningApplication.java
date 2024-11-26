2b23a97de4b538de168b0cd7df7acc35e9fc9cdd
Manash.ch <manashchoudhary123@gmail.com>
2024-11-26 10:23:47 +0530

    second commit commit

---
+++ src/main/java/com/Java8Learning/Java8LearningApplication.java
@@ -0,0 +1,13 @@
+package com.Java8Learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8LearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8LearningApplication.class, args);
+	}
+
+}
