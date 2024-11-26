1f8f7724834664a80bbe1cdeb042940a0f7b221b
Manash.ch <manashchoudhary123@gmail.com>
2024-11-26 19:12:02 +0530

    sorry hello

--- src/main/java/com/Java8Learning/Runners.java
+++ src/main/java/com/Java8Learning/Runners.java
@@ -1,11 +1,14 @@
 package com.Java8Learning;
 
 
+import com.Java8Learning.Entitys.Employee1;
 import com.Java8Learning.Repositerys.Employee1Repository;
 import com.Java8Learning.Services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Component
@@ -22,11 +25,12 @@ public class Runners implements CommandLineRunner {
 
         List<Employee1> employee1s = employeeService.getallEmployeeSalary();
         System.out.println("manash is good boy ");
+        System.out.println("susant is a good boy");
         System.out.println("ramesh and laxmi");
-        Sysout.out.println("susant is a good boy");
- System.out.println("manash is good boy ");
+
+        System.out.println("manash is good boy ");
+        System.out.println("susant is a good boy");
         System.out.println("ramesh and laxmi");
-        Sysout.out.println("susant is a good boy");
 
     }
 
